db8841fd757b86978723e19ffd388a8fbd0124e1
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2024-01-06 17:16:34 +0530

    font update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,35 +8,35 @@ const Login = () => {
       <Navbar />
       <div className="min-h-screen flex justify-center flex-col md:flex-row font-CRound ">
         <div className="bg-violet-700 flex justify-center items-center flex-1 flex-col gap-3 ">
-          <p className="text-white font-semibold text-6xl">
+          <p className="text-white font-semibold md:text-6xl text-4xl">
             For <span className="">Users</span>
           </p>
           <Link
             to="/user-login"
-            className=" text-violet-900 bg-white py-3 px-4 text-xl font-medium"
+            className=" text-violet-900 bg-white py-3 px-4 md:text-xl text-lg font-medium"
           >
             Login
           </Link>
-          <div className="text-white text-xl">
+          <div className="text-white md:text-xl text-lg">
             Not a member yet?{" "}
             <Link
               to="/user-signup"
-              className="font-semibold cursor-pointer border-b-violet-200 text-violet-100 text-2xl border-b-2"
+              className="font-semibold cursor-pointer border-b-violet-200 text-violet-100 text-lg md:text-2xl border-b-2"
             >
               SignUp Now!
             </Link>
           </div>
         </div>
         <div className="flex-1 flex justify-center items-center flex-col gap-3 bg-violet-100">
-          <p className="text-violet-700 font-semibold text-6xl">
+          <p className="text-violet-700 font-semibold md:text-6xl text-4xl">
             For <span className="">Vendors</span>
           </p>
-          <button className=" bg-violet-700  px-4 py-3 text-white font-medium text-xl">
+          <button className=" bg-violet-700  px-4 py-3 text-white font-medium md:text-xl text-lg">
             Login
           </button>
-          <div className="text-xl text-violet-700">
+          <div className="text-lg md:text-xl text-violet-700">
             Not a member yet?{" "}
-            <span className="font-semibold cursor-pointer border-b-2 border-violet-700 text-2xl">
+            <span className="font-semibold cursor-pointer border-b-2 border-violet-700 md:text-2xl text-lg">
               SignUp Now!
             </span>
           </div>
