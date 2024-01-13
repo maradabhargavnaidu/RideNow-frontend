44a025030b8cc89c38a8bfd21b371e8ff521efa2
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2024-01-14 00:07:36 +0530

    baseurl - update

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  // baseURL: `https://ridenow-backend.onrender.com`,
-  baseURL: `http://localhost:3500`,
+  baseURL: `https://ridenow-backend.onrender.com`,
+  // baseURL: `http://localhost:3500`,
 });
 export default api;
